df898eb039d97bc7ce74ee96faad3e6e16b20f9d
DevJac <dbuttonz@gmail.com>
2024-06-07 12:17:47 -0600

    022 Global Vs Local Rect and Mouse: demonstrates local vs global and rotation

---
+++ 022_global_vs_local_rect_and_mouse/color_rect.gd
@@ -0,0 +1,18 @@
+extends ColorRect
+
+
+func _process(_delta: float) -> void:
+	$LocalDebugLabel.text = (
+		'Local Rect: %20s  Local Mouse Position: %20s  Contains Mouse: %6s' %
+		[
+			get_rect(),
+			get_local_mouse_position() + get_rect().position,
+			get_rect().has_point(get_local_mouse_position() + get_rect().position),
+		])
+	$GlobalDebugLabel.text = (
+		'Global Rect: %20s  Global Mouse Position: %12s  Contains Mouse: %6s' %
+		[
+			get_global_rect(),
+			get_global_mouse_position(),
+			get_global_rect().has_point(get_global_mouse_position()),
+		])
