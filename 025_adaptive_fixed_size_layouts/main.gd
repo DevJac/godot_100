831b453678eb69b02b69891df01919416736fed2
DevJac <dbuttonz@gmail.com>
2024-06-19 15:10:57 -0600

    Adaptive Fixed Size Layout looks a lot nicer, works consistently

--- 025_adaptive_fixed_size_layouts/main.gd
+++ 025_adaptive_fixed_size_layouts/main.gd
@@ -1,57 +1,92 @@
 extends Control
 
 
-const ratio_scenes = {
-	Vector2(1152.0, 648.0): 'res://wide.tscn',
-	Vector2(648.0, 1152.0): 'res://tall.tscn',
-}
-
-
-var current_scene: String = 'res://wide.tscn';
+var current_layout: Control
 
 
 func _ready() -> void:
 	get_window().connect('size_changed', on_size_changed)
-	sync_scene_with_size()
+	sync_layout_with_size()
 
 
 func on_size_changed():
-	sync_scene_with_size()
+	sync_layout_with_size()
 
 
-class SizeScene:
+class SizeLayout:
 	var size: Vector2
-	var scene: String
+	var layout: Control
 
-	func _init(_size: Vector2, _scene: String) -> void:
+	func _init(_size: Vector2, _layout: Control) -> void:
 		self.size = _size
-		self.scene = _scene
+		self.layout = _layout
 
 
-func nearest_ratio_scene(target_ratio, available_ratio_scenes) -> SizeScene:
-	var nearest_scene_distance: float = INF
-	var nearest_scene: String;
+func nearest_ratio_layout(target_ratio, available_layouts) -> SizeLayout:
+	var nearest_layout_distance: float = INF
+	var nearest_layout: Control;
 	var nearest_size: Vector2;
-	for scene_size in available_ratio_scenes:
-		var r: float = scene_size.x / scene_size.y
-		if abs(target_ratio - r) < nearest_scene_distance:
-			nearest_scene_distance = abs(target_ratio - r)
-			nearest_scene = available_ratio_scenes[scene_size]
-			nearest_size = scene_size
-	return SizeScene.new(nearest_size, nearest_scene)
+	for layout: Control in available_layouts:
+		var r: float = layout.size.x / layout.size.y
+		if abs(target_ratio - r) < nearest_layout_distance:
+			nearest_layout_distance = abs(target_ratio - r)
+			nearest_layout = layout
+			nearest_size = layout.size
+	return SizeLayout.new(nearest_size, nearest_layout)
 
 
-func sync_scene_with_size():
+func sync_layout_with_size():
 	var s := get_window().size
-	var target_scene := nearest_ratio_scene(s.x / float(s.y), ratio_scenes)
-	if current_scene != target_scene.scene:
-		current_scene = target_scene.scene
-		assert($CenterContainer.get_child_count() == 1)
-		var old_layout = $CenterContainer.get_child(0)
-		var new_layout = load(target_scene.scene).instantiate()
-		$CenterContainer.add_child(new_layout)
-		new_layout.set_squares(old_layout.take_squares())
-		$CenterContainer.remove_child(old_layout)
-		old_layout.free()
-		assert($CenterContainer.get_child_count() == 1)
-		get_window().content_scale_size = target_scene.size
+	var window_ratio := s.x / float(s.y)
+	var layouts := $CenterContainer/Layouts.get_children()
+	var target_layout := nearest_ratio_layout(window_ratio, layouts)
+	if current_layout != target_layout.layout:
+		current_layout = target_layout.layout
+		prints('new layout')
+		var ui = $CenterContainer/UI
+		var tween := create_tween()
+		tween.set_parallel()
+		tween.tween_property(
+			ui,
+			'custom_minimum_size',
+			current_layout.custom_minimum_size,
+			2)
+		tween.tween_property(
+			ui.get_node('ColorRect1'),
+			'position',
+			current_layout.get_node('ColorRect1').position,
+			2)
+		tween.tween_property(
+			ui.get_node('ColorRect1'),
+			'size',
+			current_layout.get_node('ColorRect1').size,
+			2)
+		tween.tween_property(
+			ui.get_node('ColorRect2'),
+			'position',
+			current_layout.get_node('ColorRect2').position,
+			2)
+		tween.tween_property(
+			ui.get_node('ColorRect2'),
+			'size',
+			current_layout.get_node('ColorRect2').size,
+			2)
+		tween.tween_property(
+			ui.get_node('ColorRect3'),
+			'position',
+			current_layout.get_node('ColorRect3').position,
+			2)
+		tween.tween_property(
+			ui.get_node('ColorRect3'),
+			'size',
+			current_layout.get_node('ColorRect3').size,
+			2)
+		tween.tween_property(
+			get_window(),
+			'content_scale_size',
+			Vector2i(target_layout.size),
+			2)
+		ui.get_node('ReferenceRect').position = (
+			current_layout.get_node('ReferenceRect').position)
+		ui.get_node('ReferenceRect').size = (
+			current_layout.get_node('ReferenceRect').size)
