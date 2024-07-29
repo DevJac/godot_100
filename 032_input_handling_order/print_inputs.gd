7c83b64595129870a57bef8067153b911dc9d399
DevJac <dbuttonz@gmail.com>
2024-07-29 14:13:25 -0600

    032 Input handling order

---
+++ 032_input_handling_order/print_inputs.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+# Could also be _unhandled_input
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and not event.pressed:
+		@warning_ignore('integer_division')
+		prints('== %4d ==' % (Time.get_ticks_msec() / 1000), name, event)
