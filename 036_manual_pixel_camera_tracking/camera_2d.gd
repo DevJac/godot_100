bbfb9fbb3477b7c8a0f4c814c5be9f2eaf129fe0
DevJac <dbuttonz@gmail.com>
2024-08-30 10:29:55 -0600

    036 Manual Pixel Camera Tracking, prevent camera from moving away from target due to rounding errors

--- 036_manual_pixel_camera_tracking/camera_2d.gd
+++ 036_manual_pixel_camera_tracking/camera_2d.gd
@@ -6,10 +6,12 @@ extends Camera2D
 
 
 var real_global_position := Vector2.ZERO
+var last_global_position := Vector2.ZERO
 
 
 func _ready() -> void:
 	real_global_position = target.global_position
+	last_global_position = target.global_position
 
 
 func _process(delta: float) -> void:
@@ -19,9 +21,21 @@ func _process(delta: float) -> void:
 	real_global_position = real_global_position.lerp(
 		target.real_global_position,
 		1 - exp(-delta * tracking_speed))
-	var correction: Vector2 = (
-		target.real_global_position - self.real_global_position).posmod(1)
-	global_position = (real_global_position + correction).round()
+
+	global_position = (
+		target.global_position
+		+ (self.real_global_position - target.real_global_position).round())
+	var new_distance_to_target = (
+		global_position.distance_squared_to(target.real_global_position))
+	var last_distance_to_target = (
+		last_global_position.distance_squared_to(target.real_global_position))
+	# Do not move the camera away from the target.
+	# The target can get further away from the camera by moving away from the
+	# camera, but the camera should never move away from the target.
+	# This prevents jitter.
+	if new_distance_to_target > last_distance_to_target:
+		global_position = last_global_position
+	last_global_position = global_position
 	assert(global_position == global_position.round())
 	prints('p_pos: %-20s     c_pos: %-20s     diff: %-20s' % [
 		target.global_position,
